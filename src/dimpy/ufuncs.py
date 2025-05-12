24c11f76f96416d0a1356d32ee32f621f7cbee96
Nathan Schill <nateschill@gmail.com>
2025-05-12 16:09:49 -0600

    partially implemented some ufuncs and associated tests
    - positive
    - negative
    - subtract
    - multiply

--- src/dimpy/ufuncs.py
+++ src/dimpy/ufuncs.py
@@ -1,5 +1,8 @@
+from numbers import Number
+
 import numpy as np
 
+# TODO: Access through dp.Darray?
 import dimpy.dimensioned_array as da
 
 # TODO: There's a lot of work to do with broadcasting, if even possible.
@@ -16,9 +19,18 @@ def implements(np_ufunc):
     return decorator
 
 
+@implements(np.positive)
+def _positive(input, **kwargs):
+    return da.Darray(input.num, input.dvecs)
+
+
+@implements(np.negative)
+def _negative(input, **kwargs):
+    return da.Darray(-input.num, input.dvecs)
+
+
 @implements(np.add)
-def _add(*inputs, **kwargs):
-    left, right = inputs
+def _add(left, right, **kwargs):
     # TODO: Broadcast.
     if left.dvecs != right.dvecs:
         raise ValueError("left and right dimension vectors do not match")
@@ -27,14 +39,17 @@ def _add(*inputs, **kwargs):
 
 
 @implements(np.subtract)
-def _subtract(*inputs, **kwargs):
-    # TODO: This ain't right... is it?
-    return _add(*inputs, **kwargs)
+def _subtract(left, right, **kwargs):
+    return _add(left, -right, **kwargs)
 
 
 @implements(np.multiply)
-def _multiply(*inputs, **kwargs):
-    left, right = inputs
+def _multiply(left, right, **kwargs):
+    if isinstance(left, Number):
+        left, right = right, left
+
+    if isinstance(right, Number):
+        return da.Darray(left.num * right, left.dvecs)
 
     if left.basis != right.basis:
         raise ValueError("operands use different bases")
